5ccdbe851611cb4c7f2c14b53a2638eca273514e
lilla <ilyas.lilla@gmail.com>
2022-05-16 17:38:44 -0400

    TestNG annotations are added

--- src/test/java/com/mil/mavenGitHub/FacebookTest.java
+++ src/test/java/com/mil/mavenGitHub/FacebookTest.java
@@ -3,6 +3,8 @@ package com.mil.mavenGitHub;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,23 +12,30 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class FacebookTest {
 	public static WebDriver driver;
 
-	@Test
-	public void facebookTest() {
+	@BeforeMethod
+	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
+	}
 
+	@Test
+	public void facebookTitleTest() {
 		String title = driver.getTitle();
 		System.out.println("Facebook page title is: " + title);
 		Assert.assertEquals(title, "Facebook - log in or sign up", "Facebook title is mismatched");
-		
+	}
+
+	@Test
+	public void facebookUrlTest() {
 		String url = driver.getCurrentUrl();
 		System.out.println("Facebook page url is: " + url);
 		Assert.assertEquals(url, "https://www.facebook.com/", "Facebook url is mismatched");
-
-		driver.quit();
 	}
 
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
 }
